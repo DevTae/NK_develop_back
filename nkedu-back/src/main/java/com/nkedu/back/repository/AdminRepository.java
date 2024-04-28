40af026184030a3d632dd5fbaef34cf0b5aca02a
DevTae <dev.taehyeon.kim@gmail.com>
2024-04-28 14:27:19 +0900

    Update codes to add findAll for pageable on every repository

--- nkedu-back/src/main/java/com/nkedu/back/repository/AdminRepository.java
+++ nkedu-back/src/main/java/com/nkedu/back/repository/AdminRepository.java
@@ -4,6 +4,7 @@ package com.nkedu.back.repository;
 import com.nkedu.back.entity.Admin;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
--- nkedu-back/src/main/java/com/nkedu/back/repository/TeacherRepository.java
+++ nkedu-back/src/main/java/com/nkedu/back/repository/TeacherRepository.java
@@ -1,9 +1,9 @@
 package com.nkedu.back.repository;
 
-import com.nkedu.back.entity.Parent;
 import com.nkedu.back.entity.Teacher;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
